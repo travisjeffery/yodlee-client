51922fca2b817feafe935542fa15ebcd2b8694f8
Travis Jeffery <tj@travisjeffery.com>
2015-12-20 03:55:03 -0500

    add fn to get user session token

--- client/client.go
+++ client/client.go
@@ -7,8 +7,9 @@ import (
 )
 
 type Client struct {
-	Login    string
-	Password string
+	Login        string
+	Password     string
+	SessionToken string
 }
 
 func New(login, password string) *Client {
@@ -16,6 +17,15 @@ func New(login, password string) *Client {
 		Login:    login,
 		Password: password,
 	}
+}
+
+func (c *Client) Authenticate() []error {
+	token, errs := c.GetCobSessionToken()
+	if errs != nil {
+		return errs
+	}
+	c.SessionToken = token
+	return nil
 }
 
 func (c *Client) GetCobSessionToken() (string, []error) {
@@ -37,4 +47,28 @@ func (c *Client) GetCobSessionToken() (string, []error) {
 		return "", []error{err}
 	}
 	return j.CobrandConversationCredentials.SessionToken, nil
+}
+
+func (c *Client) GetUserSessionToken(login, password string) (string, []error) {
+	req := gorequest.New()
+	_, body, errs := req.Post("https://rest.developer.yodlee.com/services/srest/restserver/v1.0/authenticate/login").
+		Type("form").
+		Send("login=" + login).
+		Send("password=" + password).
+		Send("cobSessionToken=" + c.SessionToken).
+		End()
+	if errs != nil {
+		return "", errs
+	}
+	var j struct {
+		UserContext struct {
+			ConversationCredentials struct {
+				SessionToken string
+			}
+		}
+	}
+	if err := json.Unmarshal([]byte(body), &j); err != nil {
+		return "", []error{err}
+	}
+	return j.UserContext.ConversationCredentials.SessionToken, nil
 }
--- client/client_test.go
+++ client/client_test.go
@@ -11,13 +11,22 @@ var c *Client
 
 func init() {
 	c = &Client{
-		Login:    os.Getenv("YODLEE_LOGIN"),
-		Password: os.Getenv("YODLEE_PASSWORD"),
+		Login:    os.Getenv("YODLEE_COB_LOGIN"),
+		Password: os.Getenv("YODLEE_COB_PASSWORD"),
 	}
 }
 
 func TestGetCobSessionToken(t *testing.T) {
 	token, err := c.GetCobSessionToken()
 	assert.Nil(t, err)
-	assert.NotEmpty(t, token, token)
+	assert.NotEmpty(t, token)
+}
+
+func TestGetUserSessionToken(t *testing.T) {
+	c.Authenticate()
+	login := os.Getenv("YODLEE_USER_LOGIN")
+	pass := os.Getenv("YODLEE_USER_PASSWORD")
+	token, err := c.GetUserSessionToken(login, pass)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, token)
 }
